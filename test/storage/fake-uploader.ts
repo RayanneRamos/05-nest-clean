e4daa9b41d802792e87a8cb40b43640e5cfa60c5
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-07-01 10:33:00 -0300

    testando caso de uso de upload

---
+++ test/storage/fake-uploader.ts
@@ -0,0 +1,25 @@
+import { randomUUID } from "crypto";
+import {
+  UploadParams,
+  Uploader,
+} from "src/domain/forum/application/storage/uploader";
+
+interface Upload {
+  fileName: string;
+  url: string;
+}
+
+export class FakeUploader implements Uploader {
+  public uploads: Upload[] = [];
+
+  async upload({ fileName }: UploadParams): Promise<{ url: string }> {
+    const url = randomUUID();
+
+    this.uploads.push({
+      fileName,
+      url,
+    });
+
+    return { url };
+  }
+}

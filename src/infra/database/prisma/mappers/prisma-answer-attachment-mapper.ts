960b46c7ceeceec6029b8a4df4393b058e5ffa8c
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-07-15 15:47:37 -0300

    respostas com anexos

--- src/infra/database/prisma/mappers/prisma-answer-attachment-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-answer-attachment-mapper.ts
@@ -1,4 +1,4 @@
-import { Attachment as PrismaAttachmnent } from "@prisma/client";
+import { Prisma, Attachment as PrismaAttachmnent } from "@prisma/client";
 import { UniqueEntityID } from "src/core/entities/unique-entity-id";
 import { AnswerAttachment } from "src/domain/forum/enterprise/entities/answer-attachment";
 import { Slug } from "src/domain/forum/enterprise/entities/value-objects/slug";
@@ -16,5 +16,24 @@ export class PrismaAnswerAttachmentMapper {
       },
       new UniqueEntityID(raw.id)
     );
+  }
+
+  static toPrismaUpdateMany(
+    attachments: AnswerAttachment[]
+  ): Prisma.AttachmentUpdateManyArgs {
+    const attachmentIds = attachments.map((attachment) => {
+      return attachment.attachmentId.toString();
+    });
+
+    return {
+      where: {
+        id: {
+          in: attachmentIds,
+        },
+      },
+      data: {
+        answerId: attachments[0].answerId.toString(),
+      },
+    };
   }
 }
